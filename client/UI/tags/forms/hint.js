a0cfe76438d7afd34c7e347043c0fe55f43fd58f
Arnaud Doyen <arnaud.doyen@a-kyma.com>
2020-04-24 16:44:43 +0200

    [Bk] Update some other js (hint / placeholder) and begin Lifecycle

--- client/UI/tags/forms/hint.js
+++ client/UI/tags/forms/hint.js
@@ -5,7 +5,7 @@ Template._hint.helpers({
   hint() {
     let hint;
     if (this._pref.noHint) { return; }
-    const key = this.model._type + "." + this.field + ".hint";
+    const key = this.model.constructor.getName() + "." + this.field + ".hint";
     const translation = I18n.t(key);
     if (translation !== key) { hint = translation; }
     return hint;
--- client/UI/tags/forms/placeholder.js
+++ client/UI/tags/forms/placeholder.js
@@ -1,5 +1,7 @@
+import I18n from "../../../../lib/classes/i18n";
+
 Template.registerHelper('placeholder', function() {
-  const key = this.model._type + "." + this.field + ".placeholder";
+  const key = this.model.constructor.getName() + "." + this.field + ".placeholder";
   const translation = I18n.t(key);
   let placeholder = translation;
   if (this.noPlaceholder) { placeholder = ""; }
