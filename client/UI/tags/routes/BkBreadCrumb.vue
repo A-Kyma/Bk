45bf7656c4acd7a0c439dc7e0ccc379c3e600dfd
jerome <ferriere.jerome@gmail.com>
2020-11-22 22:11:35 +0100

    [Bk-Doc] init project

---
+++ client/UI/tags/routes/BkBreadCrumb.vue
@@ -0,0 +1,30 @@
+<template>
+    <b-breadcrumb>
+        <b-breadcrumb-item href="/">
+            <b-icon icon="house-fill" scale="1.25" shift-v="1.25" aria-hidden="true"></b-icon>
+            <t>breadcrumb.home.title</t>
+        </b-breadcrumb-item>
+        <b-breadcrumb-item v-for="item in RouteArray">
+            <t>breadcrumb.{{ item }}.title</t>
+        </b-breadcrumb-item>
+    </b-breadcrumb>
+</template>
+<script>
+    export default {
+        name: "BkBreadCrumb",
+        computed: {
+            RouteArray() {
+                let routeStr = this.$route.fullPath.substring(1);
+                if (routeStr !== ""){
+                    let routeArr = routeStr.split("/");
+                    if (routeArr.length > 0){
+                        return routeArr;
+                    }
+                }
+            }
+        },
+    }
+</script>
+<style scoped>
+
+</style>
