c01b98165b6fa92f69e29f649df11f095b3fade8
jerome <ferriere.jerome@gmail.com>
2020-11-27 17:22:29 +0100

    [Bk & Immo] Breadrumb manage parameters in the routes

--- client/UI/tags/routes/BkBreadCrumb.vue
+++ client/UI/tags/routes/BkBreadCrumb.vue
@@ -2,10 +2,10 @@
     <b-breadcrumb>
         <b-breadcrumb-item href="/">
             <b-icon icon="house-fill" scale="1.25" shift-v="1.25" aria-hidden="true"></b-icon>
-            <t>breadcrumb.home.title</t>
+            <t>route.home.title</t>
         </b-breadcrumb-item>
         <b-breadcrumb-item v-for="item in RouteArray" active>
-            <t>breadcrumb.{{ item }}.title</t>
+            <t>{{ item }}</t>
         </b-breadcrumb-item>
     </b-breadcrumb>
 </template>
@@ -16,13 +16,36 @@
         name: "BkBreadCrumb",
         computed: {
             RouteArray() {
-                let routeStr = this.$route.fullPath.substring(1);
-                if (routeStr !== ""){
-                    let routeArr = routeStr.split("/");
-                    if (routeArr.length > 0){
-                        return routeArr;
+                if (this.$route.matched.length > 0){
+                    let routeStr = this.$route.matched[0].path.substring(1);
+                    let param = this.$route.params
+                    let title = this.$route.meta.title
+                    if (routeStr !== ""){
+                        let routeArr = routeStr.split("/");
+                        if (routeArr.length > 0){
+                            for (let i = 0; i < routeArr.length; i++) {
+                                console.log(this.$route)
+                                if (routeArr[i].indexOf(":") > -1){
+                                    if(routeArr[i] === ":id"){
+                                        if (title.indexOf(".") > -1){
+                                            routeArr[i] = title
+                                        }else{
+                                            routeArr[i] = "route." + title + ".title"
+                                        }
+
+                                    }else{
+                                        let paramValue = param[routeArr[i].replace(":","")]
+                                        routeArr[i] = "route." + paramValue + ".title"
+                                    }
+                                }else{
+                                    routeArr[i] = "route." + routeArr[i] + ".title"
+                                }
+                            }
+                            return routeArr;
+                        }
                     }
                 }
+
             }
         },
     }
