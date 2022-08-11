1a177d2f5dfbf1471a1007c0e18d6f16143e8f1b
arnaud <adoyen@perso.be>
2022-08-11 10:12:45 +0200

    Device can now sendNotification from Class itself and Device is shared in Bk package

--- server/classes/device.js
+++ server/classes/device.js
@@ -17,30 +17,35 @@ Device.extend({
         },
         sendNotification(data){
             //no callback. error handled here
-            const api = new Api()
-            api.url = 'https://onesignal.com/api/v1/notifications'
-            api.method = 'POST'
-            api.authorization = Meteor.settings.oneSignal.authorisation
-
-            api.callService(data)
-                .then(result => {
-                    if (result.errors){
-                        // Check id that has become invalid
-                        let invalid_ids = result.errors.invalid_player_ids
-                        if (Array.isArray(invalid_ids) && invalid_ids.length > 0) {
-                            invalid_ids.forEach(id => {
-                                const user = Meteor.users.findOne({"profile.devices.id": id})
-                                if (!user) return
-                                let i = user.profile.devices.findIndex(device => device.id === id)
-                                if (i === undefined) return
-                                Meteor.users.update(user._id, {
-                                    ["profile.devices."+i+".send_notification"]: false
-                                })
-                            })
-                        } else
-                            throw new Meteor.Error(500, JSON.stringify(result.errors))
-                    }
-                });
+            return Device.sendNotification(data)
         }
     }
 })
+
+Device.sendNotification = function(data) {
+    //no callback. error handled here
+    const api = new Api()
+    api.url = 'https://onesignal.com/api/v1/notifications'
+    api.method = 'POST'
+    api.authorization = Meteor.settings.oneSignal.authorisation
+
+    api.callService(data)
+      .then(result => {
+          if (result.errors){
+              // Check id that has become invalid
+              let invalid_ids = result.errors.invalid_player_ids
+              if (Array.isArray(invalid_ids) && invalid_ids.length > 0) {
+                  invalid_ids.forEach(id => {
+                      const user = Meteor.users.findOne({"profile.devices.id": id})
+                      if (!user) return
+                      let i = user.profile.devices.findIndex(device => device.id === id)
+                      if (i === undefined) return
+                      Meteor.users.update(user._id, {
+                          ["profile.devices."+i+".send_notification"]: false
+                      })
+                  })
+              } else
+                  throw new Meteor.Error(500, JSON.stringify(result.errors))
+          }
+      })
+}
